3b5c484a2b5436b2cc251320a59bf720c2f9124d
britellington <114776003+britellington@users.noreply.github.com>
2022-10-05 11:47:15 -0600

    Update Final_Pi_Code.py

--- Working Code/Final_Pi_Code.py
+++ Working Code/Final_Pi_Code.py
@@ -1,7 +1,4 @@
-# Abraham Sitanggang
-# Mini Project CV Code
-
-# import the necessary packages
+# import the necessary libraries 
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
@@ -13,8 +10,8 @@ import smbus
 import smbus2
 import board
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
-#Python code for pi-
-#take the setpoint that the pi reads from the aruco marker and send it to arduino
+
+#lcd stuff
 
 lcd_columns = 16
 lcd_rows = 2
@@ -23,17 +20,19 @@ i2c = board.I2C()
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 lcd.clear()
 
+#address information for communication 
 abus = smbus.SMBus(1)
 address = 0x2a
 slave_address = 20
-angle = 0
 
+#initlize variables 
+angle = 0
 data = 0
 
-
-def writeNumber(variableName):
+#write information to arduino/lcd and print if there are errors
+def writeNumber(writeThis):
     try:
-        abus.write_byte(address,variableName)
+        abus.write_byte(address,writeThis)
     except:
         print('io error')
     
@@ -47,13 +46,13 @@ def writeNumber(variableName):
         print('io error')
     
     tpos = ''
-    if ( variableName == 0 ):
+    if ( writeThis == 0 ):
         tpos = '00'
-    elif ( variableName == 1 ):
+    elif ( writeThis == 1 ):
         tpos = '090'
-    elif ( variableName == 2 ):
+    elif ( writeThis == 2 ):
         tpos = '180'
-    elif ( variableName == 3 ):
+    elif ( writeThis == 3 ):
         tpos = '270'
     lcd.message = 'Curr: ' + str(data) + '\nTest: ' + str(tpos) 
     
@@ -61,7 +60,7 @@ def writeNumber(variableName):
 
     
 
-
+#lcd setpoint funcction
 def readLCD(setpoint):
     
     setPrint = 'Setpoint: ' + str(setpoint)
@@ -70,67 +69,62 @@ def readLCD(setpoint):
 
 
 
-# Configure camera to take a video
+#Start continous capture 
 cap = cv2.VideoCapture(0)
 while(True):
-    #Capture frame-by-frame
+  
     ret, frame = cap.read()
-    #Our operations on the frame come here
+    #convert to grey scale and find center of the camera
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Obtain the image pixel size, and find the horizontal (x-coordinate) center
     imgSize = img.shape
-    xCoord = (imgSize[1] / 2)
-    yCoord = (imgSize[0] / 2)
-    # Import the dictonary of markers, define the parameters, and load values into vars
+    xSize = (imgSize[1] / 2)
+    ySize = (imgSize[0] / 2)
+    
+    #initilize parameters and select an aruco dictionary 
     arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
     arucoParams = cv2.aruco.DetectorParameters_create()
     (corners, ids, rejected) = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
-    # Verify if an ArUco marker is detected
+    
+    #enter loop if an id is detected 
     if len(corners)> 0:
-        # flatten ID list
         ids = ids.flatten()
-        # loop over the marker coners
+
         for (markerCorner, markerID) in zip(corners, ids):
-            # extract marker corners and convert them to integers
             corners = markerCorner.reshape((4,2))
             (topLeft, topRight, bottomRight, bottomLeft) = corners
             topRight = (int(topRight[0]), int(topRight[1]))
             bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
-            #draw bounding box
-            cv2.line(img, topLeft, topRight, (0, 255, 0), 2)
-            cv2.line(img, topRight, bottomRight, (0, 255, 0), 2)
-            cv2.line(img, bottomRight, bottomLeft, (0, 255, 0), 2)
-            cv2.line(img, bottomLeft, topLeft, (0, 255, 0), 2)
-            # compute and draw the center (x, y)-coordinates of the ArUco marker
-            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            #add drawing overlay to the image 
+            cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+            
+            # find the center of the marker 
+            pX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            pY = int((topLeft[1] + bottomRight[1]) / 2.0)
             cv2.circle(img, (cX, cY), 4, (0, 0, 255), -1)
-            # Calculate the angle from camera to the object by using the provided formula
-            ## angle = round( (57/2) * ( (cX - xCoord) / xCoord), 2 )
-             # Use pixel manipulation to determine quadrants, then send the angle corresponding to each quadrant to the arduino
-            if cX > xCoord and cY < yCoord:
+    
+            # determine the quadrant 
+            if pX > xSize and pY < ySize:
                 angle = 1
-            elif cX > xCoord and cY > yCoord:
+            elif pX > xSize and pY > ySize:
                 angle = 2
-            elif cX < xCoord and cY > yCoord:
+            elif pX < xSize and pY > ySize:
                 angle = 3
-            elif cX < xCoord and cY < yCoord:
+            elif pX < xSize and pY < ySize:
                 angle = 0
-            # Display the ID's and angle 
+            # Display on frame
             anglePrint = str(angle) + ' degrees'
-            #cv2.putText(img, str(markerID), (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)
-            cv2.putText(img, anglePrint, (topRight[0], topRight[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)
+            cv2.putText(frame, anglePrint, (topRight[0], topRight[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)
             writeNumber(angle)
-            #print(angle)
-            #currentPos = abus.read_byte(slave_address)
-#           readNumber()
-            #readLCD(angle)
-        # show the output image
-        cv2.imshow('Image', img)
+          
+        # show the frame 
+        cv2.imshow('Image', frame)
     else:
-        cv2.imshow('Image', img)
+        cv2.imshow('Image', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
